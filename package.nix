67b00f3d3bf24edb94acd8ab21706218c1e789a5
Mert Kurttutan <kurttutan.mert@gmail.com>
2026-02-07 14:29:29 +0300

    Make zed available as a desktop app

--- package.nix
+++ package.nix
@@ -1,17 +1,18 @@
-{ lib
-, stdenv
-, fetchurl
-, autoPatchelfHook
-, alsa-lib
-, fontconfig
-, gnutar
-, gzip
-, libxkbcommon
-, makeWrapper
-, openssl
-, vulkan-loader
-, wayland
-, xorg
+{
+  lib,
+  stdenv,
+  fetchurl,
+  autoPatchelfHook,
+  alsa-lib,
+  fontconfig,
+  gnutar,
+  gzip,
+  libxkbcommon,
+  makeWrapper,
+  openssl,
+  vulkan-loader,
+  wayland,
+  xorg,
 }:
 
 let
@@ -65,13 +66,40 @@ stdenv.mkDerivation {
     fi
 
     makeWrapper "$zed_bin" "$out/bin/zed" \
-      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [
-        vulkan-loader
-        wayland
-        libxkbcommon
-        xorg.libX11
-        xorg.libxcb
-      ]}
+      --prefix LD_LIBRARY_PATH : ${
+        lib.makeLibraryPath [
+          vulkan-loader
+          wayland
+          libxkbcommon
+          xorg.libX11
+          xorg.libxcb
+        ]
+      }
+
+    mkdir -p "$out/share/applications"
+    cat > "$out/share/applications/zed.desktop" <<'EOF'
+    [Desktop Entry]
+    Version=1.0
+    Type=Application
+    Name=$APP_NAME
+    GenericName=Text Editor
+    Comment=A high-performance, multiplayer code editor.
+    TryExec=$APP_CLI
+    StartupNotify=$DO_STARTUP_NOTIFY
+    Exec=$APP_CLI $APP_ARGS
+    Icon=$APP_ICON
+    Categories=Utility;TextEditor;Development;IDE;
+    Keywords=zed;
+    # To add Zed to "Open Folder With..." context menu, add `inode/directory` to the MimeType field (semicolon separated)
+    # Arch linux users have reported this setting Zed as default file browser. See https://github.com/zed-industries/zed/pull/39076 and related issues.
+    # If this happens to you, an unconfirmed fix may be to install Arch's `gnome-defaults-list` package.
+    MimeType=text/plain;application/x-zerosize;x-scheme-handler/zed;
+    Actions=NewWorkspace;
+
+    [Desktop Action NewWorkspace]
+    Exec=$APP_CLI --new $APP_ARGS
+    Name=Open a new workspace
+
     runHook postInstall
   '';
 

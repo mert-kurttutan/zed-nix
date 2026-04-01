1d5460c00ddbe63c23bca6e623f2a1142ebb6bf2
mert-kurttutan <88637659+mert-kurttutan@users.noreply.github.com>
2026-04-01 16:24:13 +0000

    chore: update codex to version 0.230.0

--- package.nix
+++ package.nix
@@ -17,10 +17,10 @@
 }:
 
 let
-  version = "0.229.0";
+  version = "0.230.0";
   src = fetchurl {
     url = "https://github.com/zed-industries/zed/releases/download/v${version}/zed-linux-x86_64.tar.gz";
-    sha256 = "sha256-GA9N6veqUTo/BmSOPsqtJwv/zGRUHoV/HnA4EHGpCMs=";
+    sha256 = "sha256-ZyCsc+K6JXoLRGCmdvJ/MXMiqU9fnKnJjyRqMovJ7Qw=";
   };
 
 in

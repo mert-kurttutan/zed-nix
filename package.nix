48226cd17fdc0c55970cc71ab5d456945f48a521
mert-kurttutan <88637659+mert-kurttutan@users.noreply.github.com>
2026-05-06 16:36:23 +0000

    chore: update zed to version 1.1.5

--- package.nix
+++ package.nix
@@ -17,10 +17,10 @@
 }:
 
 let
-  version = "1.0.1";
+  version = "1.1.5";
   src = fetchurl {
     url = "https://github.com/zed-industries/zed/releases/download/v${version}/zed-linux-x86_64.tar.gz";
-    sha256 = "sha256-3E4uNzdYy5HPlMhuW0OUIWlkwmo4RlPlrJj1rjlBPIU=";
+    sha256 = "sha256-qWcZU3dNlA+wldMl7cXLJu9xKJqYRnYu25YQpWmyJEY=";
   };
 
 in

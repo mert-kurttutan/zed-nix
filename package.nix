ff19cbdbadb82d9b0ae04af2d69dba8301ce7733
mert-kurttutan <88637659+mert-kurttutan@users.noreply.github.com>
2026-07-20 16:30:35 +0000

    chore: update zed to version 1.10.2

--- package.nix
+++ package.nix
@@ -17,10 +17,10 @@
 }:
 
 let
-  version = "1.11.3";
+  version = "1.10.2";
   src = fetchurl {
     url = "https://github.com/zed-industries/zed/releases/download/v${version}/zed-linux-x86_64.tar.gz";
-    sha256 = "sha256-SD7gnIhYAd16AjDjzPXEY8Tb5FWZJ/WFb9Vo57GjLj4=";
+    sha256 = "sha256-72j04wSpRP7N5ELzG88SjynTWePLCO28kcmQMqf8enU=";
   };
 
 in
